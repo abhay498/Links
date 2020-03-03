a531de43ded9849c9dd9c2644a3e7e763559b74b
Abhay Kumar Singh <abskumar798@gmail.com>
2020-03-03 23:19:26 +0530

    Update links.py

--- links.py
+++ links.py
@@ -1,6 +1,7 @@
 
 def main():
   print('OpenCV link : ', 'https://www.youtube.com/watch?v=-ZrDjwXZGxI')
+  print('plotly : ', 'https://www.youtube.com/playlist?list=PLH6mU1kedUy9HTC1n9QYtVHmJRHQ97DBa')
   
 if __name__ == '__main__':
     main()
