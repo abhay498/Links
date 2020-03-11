9cea5ce54c018cfe255bd67fe27ec66b1a5db223
Abhay Kumar Singh <abskumar798@gmail.com>
2020-03-12 01:48:06 +0530

    Update links.py

--- links.py
+++ links.py
@@ -2,6 +2,7 @@
 def main():
   print('OpenCV link : ', 'https://www.youtube.com/watch?v=-ZrDjwXZGxI')
   print('plotly : ', 'https://www.youtube.com/playlist?list=PLH6mU1kedUy9HTC1n9QYtVHmJRHQ97DBa')
+  print('Beautiful code : ', 'https://github.com/lancetw/ebook-1/blob/master/09_other/%E4%BB%A3%E7%A0%81%E4%B9%8B%E7%BE%8EBeautiful%20Code.pdf')
   
 if __name__ == '__main__':
     main()
